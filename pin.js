3b049c629cb297a2f1d1b39a132ffd78fa14a209
dogpantz <dogpantz@gmai.com>
2013-08-13 22:10:09 -0600

    Finished basic architecture in pin.js. More details to be added.

--- pin.js
+++ pin.js
@@ -1,27 +1,68 @@
-function BasePin(_x, _y, _options) {
-    return {
-        x: _x, y: _y,
+var PinAssets = {};
 
-
-        init: function() {
-
-        },
-
-        update: function(delta) {
-
-        },
-
-        render: function(gc) {
-
-        }
-    };
-
-}
+/* ### Pin Shooter
+ */
 
 function PinShooter(_x, _y, _options) {
     return {
         x: _x, y: _y,
 
+        shoot: function() {
+
+        },
+
+        onMouse: function() {
+
+        },
+
+        init: function() {
+
+        },
+
+        update: function(delta) {
+
+        },
+
+        render: function(gc) {
+            var img = PinAssets.arrow;
+            gc.drawImage(img, this.x, this.y);
+        }
+    };
+
+}
+
+/* ### Pins
+ */
+
+function Pin(x, y, options) {
+    var base = BasePin(x, y, options);
+
+    var type = options.type || "standard";
+
+    switch(type) {
+        case "standard":
+            return Pin.Standard(base);
+            break;
+
+    }
+}
+
+Pin.Standard = function(base) {
+
+    base.render = function(gc) {
+        var img = PinAssets.pin;
+        gc.drawImage(img, base.x, base.y);
+    };
+
+    return base;
+
+};
+
+Pin.Base = function(_x, _y, options) {
+    return {
+        x: _x, y: _y,
+
+
         init: function() {
 
         },
@@ -35,4 +76,5 @@ function PinShooter(_x, _y, _options) {
         }
     };
 
-}
+};
+
